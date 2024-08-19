b9f1dce766d4e8958da5b3c37ea1540afd3ead77
kokolopo <kokolopo321@gmail.com>
2024-08-19 18:59:10 +0700

    a

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		// Set cookie
 		c.Cookie(&fiber.Cookie{
 			Name:  "auth_tkt",
-			Value: "a2082928acb5ab25d7200c00bb534c9458c25b3621763989bbaef712dd80d806081773d815436bf89ccbf6220b1e67befb5788fcab4db5d5a007a7a7e40274dc66c32a39197!userid_type:int",
+			Value: "765f33ff08199a88d15e345708234871fd9131200b90e4c0031bf7055f08ebac59daace98e2fc091e6bc32308e91a03c0b8b1eb01372cb0dde1b71e563e676fe66c332ed197!userid_type:int",
 			// Value:  "8dde60729f8adb12881f9de0675d5325628b6762d25a967416f2fa7b980368f499a47eda6897fe205ae7bf41e07f889038a3af622de83cd1259da0f7b0b08b9f66bbae76197!userid_type:int",
 			Domain: "simasda.kotabogor.go.id",
 			Path:   "/",
