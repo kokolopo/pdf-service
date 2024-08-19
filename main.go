dd10c0db091d3c7bb8d765b9bcf11b8c8b88aa0a
kokolopo <kokolopo321@gmail.com>
2024-08-19 20:25:20 +0700

    s

--- main.go
+++ main.go
@@ -12,17 +12,28 @@ func main() {
 	app.Get("/set-pdf/:penetapan_id", func(c *fiber.Ctx) error {
 		penetapanID := c.Params("penetapan_id")
 
+		c.Cookie(&fiber.Cookie{
+			Name:     "WhatEver",
+			Value:    "0f78c540a08c25d497e42b1c52100c1875273d4906a3d172a9554c20a4da594930e3824b",
+			Domain:   "simasda.kotabogor.go.id",
+			Path:     "/",
+			SameSite: "Lax",
+		})
+
 		// Set cookie
 		c.Cookie(&fiber.Cookie{
-			Name:  "auth_tkt",
-			Value: "765f33ff08199a88d15e345708234871fd9131200b90e4c0031bf7055f08ebac59daace98e2fc091e6bc32308e91a03c0b8b1eb01372cb0dde1b71e563e676fe66c332ed197!userid_type:int",
-			// Value:  "8dde60729f8adb12881f9de0675d5325628b6762d25a967416f2fa7b980368f499a47eda6897fe205ae7bf41e07f889038a3af622de83cd1259da0f7b0b08b9f66bbae76197!userid_type:int",
-			Domain: "simasda.kotabogor.go.id",
-			Path:   "/",
+			Name:     "auth_tkt",
+			Value:    "55e243b09d393c81709a58779a4051067d5f265e0383dc194c160853eb5f2d572aaf37be3f38e2efe259af10c20fae94d70549fe84bbcda1be6f65cff6bc683b66c3463c197!userid_type:int",
+			Domain:   "simasda.kotabogor.go.id",
+			Path:     "/",
+			SameSite: "Lax",
 		})
 
 		// Redirect to the desired URL
-		return c.Redirect("https://simasda.kotabogor.go.id/devel/aset/inventaris/kiba/lki/rpt?id=" + penetapanID)
+		// return c.Redirect("https://simasda.kotabogor.go.id/devel/aset/inventaris/kiba/lki/rpt?id=" + penetapanID)
+		return c.Redirect("https://simasda.kotabogor.go.id/inventaris/aset/inventaris/kibb/lki/rpt?id" + penetapanID)
+		// https://simasda.kotabogor.go.id/inventaris/aset/inventaris/kibb/lki/rpt?id=5730827
+		// return c.SendString("ok " + penetapanID)
 	})
 
 	log.Fatal(app.Listen(":5001"))
