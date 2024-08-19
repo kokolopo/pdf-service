6ccf4aefd09807ddca9082d5e1b5587f775b0411
kokolopo <kokolopo321@gmail.com>
2024-08-19 18:19:54 +0700

    s

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		// Set cookie
 		c.Cookie(&fiber.Cookie{
 			Name:  "auth_tkt",
-			Value: "62fd5c685d450a356af16ccd56392496be85e35c5c5f5d1ceeede74bd50992b931a269a940742646f1feea8bb902cebd720c6ac50d78b469e6d8f7abf59ac96166bc51b7197!userid_type:int",
+			Value: "a2082928acb5ab25d7200c00bb534c9458c25b3621763989bbaef712dd80d806081773d815436bf89ccbf6220b1e67befb5788fcab4db5d5a007a7a7e40274dc66c32a39197!userid_type:int",
 			// Value:  "8dde60729f8adb12881f9de0675d5325628b6762d25a967416f2fa7b980368f499a47eda6897fe205ae7bf41e07f889038a3af622de83cd1259da0f7b0b08b9f66bbae76197!userid_type:int",
 			Domain: "simasda.kotabogor.go.id",
 			Path:   "/",
@@ -25,5 +25,5 @@ func main() {
 		return c.Redirect("https://simasda.kotabogor.go.id/devel/aset/inventaris/kiba/lki/rpt?id=" + penetapanID)
 	})
 
-	log.Fatal(app.Listen(":6000"))
+	log.Fatal(app.Listen(":5001"))
 }
