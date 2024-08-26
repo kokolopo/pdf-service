3ab3af80fad4569e0b32204d3c8621a7715c5c76
kokolopo <kokolopo321@gmail.com>
2024-08-26 10:43:51 +0700

    s

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		// Set cookie
 		c.Cookie(&fiber.Cookie{
 			Name:  "auth_tkt",
-			Value: "205e856cb5a1801ceef7cb83f4f5b49de153019f52f5fa3c0f07f369bf6f6bec92a52e90e29028a8b3c89ba27c2b5375516e25a5b67eae36d230d5eee870bc9066cbdbf3197!userid_type:int",
+			Value: "0c599c6ca8cdfca8ae605f0a32cca214e57c1473fad351a6a1627a5c4c457a3c3c7db4bea782ddefc43b041af026c63ef8a1571c32caeca78a363563841d875066cbf95f3!userid_type:int",
 			// Domain:   "simasda.kotabogor.go.id",
 			Path:     "/",
 			SameSite: "Lax",
@@ -23,8 +23,8 @@ func main() {
 		})
 
 		// Redirect to the desired URL
-		return c.Redirect("https://simasda.kotabogor.go.id/devel/aset/inventaris/kiba/lki/rpt?id=" + penetapanID)
-		// return c.Redirect("https://simasda.kotabogor.go.id/inventaris/aset/inventaris/kibb/lki/rpt?id" + penetapanID)
+		// return c.Redirect("https://simasda.kotabogor.go.id/devel/aset/inventaris/kiba/lki/rpt?id=" + penetapanID)
+		return c.Redirect("https://simasda.kotabogor.go.id/inventaris/aset/inventaris/kibb/lki/rpt?id" + penetapanID)
 		// https://simasda.kotabogor.go.id/inventaris/aset/inventaris/kibb/lki/rpt?id=5730827
 		// return c.SendString("ok " + penetapanID)
 	})
